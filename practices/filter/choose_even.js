74b244921a9023f8b631038d945c89dd0b964b4c
bootcamptestkatefan <kate.fan@oocl.com>
2018-11-21 14:11:03 +0800

    using for loop to pass right filter-choose-even

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -2,7 +2,13 @@
 
 function choose_even(collection) {
 
-  //implement here
+  	var result = [];
+  	for (var i = 0; i < collection.length; i++){
+   		if(collection[i]%2==0){
+  				result.push(collection[i]);
+  		}
+  	}
+    return result;
 }
 
 module.exports = choose_even;
