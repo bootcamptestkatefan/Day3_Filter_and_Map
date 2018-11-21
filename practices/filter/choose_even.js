4971f9764a31fd03b9c97035c6006084909d781f
bootcamptestkatefan <kate.fan@oocl.com>
2018-11-21 14:27:27 +0800

    try to use "filter" to do "choose_even"

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function choose_even(collection) {
+/*function choose_even(collection) {
 
   	var result = [];
   	for (var i = 0; i < collection.length; i++){
@@ -10,5 +10,28 @@ function choose_even(collection) {
   	}
     return result;
 }
+*/
+
+function choose_even(collection) {
+
+  	var result = [];
+  	for (var i = 0; i < collection.length; i++){
+   		if(isEven(collection[i])){
+  				result.push(collection[i]);
+  		}
+  	}
+    return result;
+}
+
+function isEven(number){
+  if (number%2==0){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
 
 module.exports = choose_even;
