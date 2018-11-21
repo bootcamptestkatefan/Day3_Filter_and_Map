f9c461e839da99a59f647172f1268dc3ee7f4326
bootcamptestkatefan <kate.fan@oocl.com>
2018-11-21 16:02:18 +0800

    Done lah for map_to_even.js

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,27 @@
 'use strict';
-function map_to_even(collection){
-  return [2,4,6,8,10];
+/*
+  function map_to_even(collection){
+  for (var i = 0 ; i < collection.length ; i ++){
+    collection[i] = collection[i]*2
+    }
+
+  return collection;
+  }
+*/
+
+function doMapToEven(collection , aFunctionThatYouWantToExecute){
+  for (var i = 0 ; i < collection.length ; i ++){
+    collection[i] = aFunctionThatYouWantToExecute(collection[i]);
+    }
+  return collection;
 }
+
+const times2 = function (number){
+  return number*2
+}
+
+function map_to_even(element){
+  return doMapToEven(element, times2);
+}
+
 module.exports = map_to_even;
